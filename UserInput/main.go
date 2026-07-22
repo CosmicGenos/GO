555da022a66351b34e44c3add90aade441a2661c
CosmicGenos <nimesha12417@usci.ruh.ac.lk>
2026-07-22 11:22:31 +0530

    go-4

--- UserInput/main.go
+++ UserInput/main.go
@@ -1,19 +1,34 @@
 package main
 
-import "fmt"
-import "bufio"
-import "os"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func main() {
 	fmt.Println("Hello, World!")
-	
+
 	fmt.Println("Please enter your name:")
 	reader := bufio.NewReader(os.Stdin)
-	//go dont have built-in function to read input from the user, so we use bufio.NewReader to read input from standard 
+	//go dont have built-in function to read input from the user, so we use bufio.NewReader to read input from standard
 	// input (os.Stdin). The ReadString method reads until it encounters a newline character ('\n').
 	// and go dont have  error handling, so we ignore the error returned by ReadString method using the blank identifier (_).
-	output, _ := reader.ReadString('\n') 
+	output, _ := reader.ReadString('\n')
 	fmt.Println("Hello, " + output)
-	
-	
+
+	fmt.Println("Please enter your age:")
+	ageReader := bufio.NewReader(os.Stdin)
+	ageInput, _ := ageReader.ReadString('\n')
+	fmt.Println("You are " + ageInput + " years old.")
+
+	addoneage,err :=  strconv.ParseFloat(strings.TrimSpace(ageInput), 32)
+	if err != nil {
+		fmt.Println("Error parsing age:", err)
+		return
+	}
+	fmt.Println("You are " + fmt.Sprint(addoneage) + " years old.")
+
 }
